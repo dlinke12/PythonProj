8a6ad33bf3ed98da235d3b9930c35b5af6387f8f
Derek Linke <derek.linke@gmail.com>
2019-12-12 13:07:29 -0500

    Added for loops and while loops to code

--- IfCondition.py
+++ IfCondition.py
@@ -8,3 +8,24 @@ if name: #Truthy value, when string is not blank, boolena returns true.
     print('True, value is not blank')
 else:
     print('False, value is a blank valur')
+
+spam= 0
+while spam < 5:
+        print('Hello ' + str(spam))
+        spam = spam + 1 
+
+spam = 0
+while spam < 5:
+        spam=spam+1
+        if spam == 3:
+            continue
+        print('Spam is ' + str(spam))
+        
+
+print('Mat name is')
+for i in range(5):
+    print('Jimmy Five Times: ' + str(i))
+
+print('Mat name is')
+for i in range(10,100,3):
+    print('Jimmy Five Times: ' + str(i))
