c4b48027d711e47ec63df50150c5bcfae1090aef
Derek Linke <derek.linke@gmail.com>
2019-12-12 12:47:50 -0500

    Add if else python file

---
+++ IfCondition.py
@@ -0,0 +1,10 @@
+name='Alice'
+if name == 'Alice':
+    print('Hi, Alice')
+else:
+    print('Who are you?')    
+
+if name: #Truthy value, when string is not blank, boolena returns true.
+    print('True, value is not blank')
+else:
+    print('False, value is a blank valur')
